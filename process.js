7c830243fa830c59a7cecda01ac3561a50f43849
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-29 22:45:14 +0600

    number get successfully and the < and C buttong working perfecrly for better understanding degugger console.log() is been added

--- process.js
+++ process.js
@@ -3,6 +3,33 @@ const showPin = document.getElementById('showPin');
 
 pinGenerate.addEventListener('click', function(){
     let pin = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-    console.log(pin); //debug that pin is showing
+    console.log(pin);   //debug that pin is showing
     showPin.value = pin;
 })
+
+//input from user
+const calcButtons = document.querySelectorAll('.button');
+const inputBox = document.getElementById('inputFromUser');
+
+// Add an event listener to each calculator button
+calcButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Get the value of the button
+    const value = button.textContent;
+    console.log(value); //debug
+
+    if (isNaN(value) === false) {
+        inputBox.value += value;
+    }
+
+    if(value === 'C'){
+        inputBox.value = "";
+    }
+
+    if(value === '<'){
+        inputBox.value = inputBox.value.toString().slice(0,-1);
+    }
+
+    // Append the value of the button to the input box
+  });
+});
